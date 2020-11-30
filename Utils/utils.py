f20aa5d08c29b075eabee4a8fc53944a72389864
Luis Henrique Albino <luishenriquealbino@gmail.com>
2020-11-29 21:02:06 -0300

    Iniciado o cadastro do cliente & realizado alguns ajustes

--- Utils/utils.py
+++ Utils/utils.py
@@ -4,9 +4,9 @@ import tkinter as tk
 class Utils:
 
     @staticmethod
-    def createDefaultFrame(master, padX, padY):
+    def createDefaultFrame(master, padx, pady):
         container = tk.Frame(master)
-        container["padx"] = padX
-        container["pady"] = padY
+        container["padx"] = padx
+        container["pady"] = pady
         container.pack()
         return container
