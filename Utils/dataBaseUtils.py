5e087ac3ed5c512748aacb1f92f227bf79d786e8
Luis Henrique Albino <luishenriquealbino@gmail.com>
2020-12-06 23:48:46 -0300

    Ajustes

--- Utils/dataBaseUtils.py
+++ Utils/dataBaseUtils.py
@@ -2,7 +2,7 @@ class DataBaseUtils:
     ABSOLUTE_PATH = "D:\\WorkSpacePyCharm\\getNinja\\model\\banco.db"
 
     CREATE_CLIENTE = """CREATE TABLE IF NOT EXISTS clientes (id integer primary key autoincrement, 
-         nome text not null, telefone text, cnpj_cpf Integer, cod_grupo Integer);"""
+         nome text not null, telefone text, cnpj_cpf String, cod_grupo String);"""
 
     CREATE_USER = """CREATE TABLE IF NOT EXISTS usuarios (id integer primary key autoincrement, 
         nome text not null, login text not null, senha text not null, isRoot integer);"""
@@ -24,5 +24,12 @@ class DataBaseUtils:
     SELECT_USER_BY_LOGIN = "SELECT id FROM usuarios WHERE login = :p1 AND senha = :p2"
 
     SELECT_GRUPOS = """SELECT codigo, nome FROM grupos """
+
+    SELECT_CLIENTE_BY_ID = """SELECT NOME, TELEFONE, CNPJ_CPF, COD_GRUPO FROM CLIENTES WHERE ID = :p1"""
+
+    SELECT_ALL_CLIENTES = """SELECT ID, NOME, TELEFONE, CNPJ_CPF, COD_GRUPO FROM CLIENTES"""
+
+    UPDATE_CLIENTE_BY_ID = """UPDATE CLIENTES SET NOME = :p1, TELEFONE = :p2, CNPJ_CPF = :p3, COD_GRUPO = :p4  
+                            WHERE ID = 7 """
 
     DELETE_CLIENTE = "DELETE FROM clientes WHERE id = :p1"
--- views/cadClienteView.py
+++ views/cadClienteView.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import sqlite3
 from tkcalendar import DateEntry
 from Utils.utils import Utils
@@ -13,54 +14,77 @@ class CadFuncionariosContent:
     def __init__(self, master=None):
         self.master = master
         self.master.title("Cadastro de Clientes")
-        self.master.geometry("750x500")
+        self.master.geometry("750x750")
+        self.flagOnConsulta = False
+
+        # LABELS
+        self.lb_id_cliente = tk.Label(self.master, text="Cliente ID")
+        self.lb_nome_cliente = tk.Label(self.master, text="Nome Cliente")
+        self.lb_cnpj_cliente = tk.Label(self.master, text="CNPJ/CPF")
+        self.lb_telefone_cliente = tk.Label(self.master, text="Telefone")
+        self.lb_grupo_cliente = tk.Label(self.master, text="Código Grupo")
+
+        # CAMPOS
+        self.cp_id_cliente = tk.Entry(self.master, width=10)
+        self.cp_nome_cliente = tk.Entry(self.master)
+        self.cp_cnpj_cliente = tk.Entry(self.master)
+        self.cp_telefone_cliente = tk.Entry(self.master)
+        self.cp_grupo_cliente = tk.Entry(self.master)
+
+        # BUTTONS
+        self.btn_buscar = tk.Button(self.master, text="Buscar", command=self.buscar_cliente)
+        self.btn_cadastrar = tk.Button(self.master, text="Cadastrar", command=self.cadastra_cliente)
+        self.btn_excluir = tk.Button(self.master, text="Excluir", command=self.excluir_cliente)
+        self.btn_limpar = tk.Button(self.master, text="Limpar", command=self.limpar_campos)
+        self.btn_limpar.config(state="disable")
+        self.btn_atualizar = tk.Button(self.master, text="Atualizar", command=self.atualizar_cliente)
+        self.btn_atualizar.config(state="disable")
+
+        # TREE's
+        self.tree_main = ttk.Treeview(self.master, columns=('id', 'Nome Cliente', 'Telefone'), show='headings')
+        self.buildTree()
+
         self.buildUI()
 
     def buildUI(self):
-        lb_id_cliente = tk.Label(self.master, text="Cliente ID")
-        lb_id_cliente .grid(row=0, column=0, ipadx=75, ipady=10)
+        self.lb_id_cliente .grid(row=0, column=0, ipadx=75, ipady=10)
+        self.cp_id_cliente.grid(row=0, column=1, padx=15, sticky=tk.E)
+        self.btn_buscar.grid(row=0, column=2, sticky=tk.W+tk.E)
+        self.btn_limpar.grid(row=0, column=3, sticky=tk.W + tk.E)
 
-        cp_id_cliente = tk.Entry(self.master, width=10)
-        cp_id_cliente.grid(row=0, column=1)
+        self.lb_nome_cliente.grid(row=1, column=0, ipadx=75, ipady=10)
+        self.cp_nome_cliente.grid(row=1, column=1)
 
-        lb_nome_cliente = tk.Label(self.master, text="Nome Cliente")
-        lb_nome_cliente.grid(row=1, column=0, ipadx=75, ipady=10)
+        self.lb_cnpj_cliente.grid(row=2, column=0, ipadx=75, ipady=10)
+        self.cp_cnpj_cliente.grid(row=2, column=1)
 
-        cp_nome_cliente = tk.Entry(self.master)
-        cp_nome_cliente.grid(row=1, column=1)
+        self.lb_telefone_cliente.grid(row=3, column=0, ipadx=75, ipady=10)
+        self.cp_telefone_cliente.grid(row=3, column=1)
 
-        lb_cnpj_cliente = tk.Label(self.master, text="CNPJ/CPF")
-        lb_cnpj_cliente.grid(row=2, column=0, ipadx=75, ipady=10)
+        self.lb_grupo_cliente.grid(row=4, column=0, ipadx=75, ipady=10)
+        self.cp_grupo_cliente.grid(row=4, column=1)
 
-        cp_cnpj_cliente = tk.Entry(self.master)
-        cp_cnpj_cliente.grid(row=2, column=1)
+        self.btn_cadastrar.grid(row=5, column=0)
+        self.btn_excluir.grid(row=5, column=1)
+        self.btn_atualizar.grid(row=5, column=2)
 
-        lb_telefone_cliente = tk.Label(self.master, text="Telefone")
-        lb_telefone_cliente.grid(row=3, column=0, ipadx=75, ipady=10)
-
-        cp_telefone_cliente = tk.Entry(self.master)
-        cp_telefone_cliente.grid(row=3, column=1)
-
-        lb_grupo_cliente = tk.Label(self.master, text="Código Grupo")
-        lb_grupo_cliente.grid(row=4, column=0, ipadx=75, ipady=10)
-
-        cp_grupo_cliente = tk.Entry(self.master)
-        cp_grupo_cliente.grid(row=4, column=1)
-
-        btn_cadastrar = tk.Button(self.master, text="Cadastrar", command=lambda: self.cadastra_cliente(nome=cp_nome_cliente.get(),
-                                                                                                       cnpj=cp_cnpj_cliente.get(),
-                                                                                                       telefone=cp_telefone_cliente.get(),
-                                                                                                       cod_grupo=cp_grupo_cliente.get()))
-        btn_cadastrar.grid(row=5, column=0, ipadx=75, pady=20)
-
-        btn_excluir = tk.Button(self.master, text="Excluir", command=lambda: self.excluir_cliente(cp_id_cliente.get()))
-        btn_excluir.grid(row=5, column=1)
+        self.tree_main.grid(row=6, column=0, columnspan=10)
 
         pass
 
-    def cadastra_cliente(self, nome, cnpj, telefone, cod_grupo):
+    def cadastra_cliente(self):
         msg_aviso = "Cliente cadastrado com sucesso!!"
+
+        nome = self.cp_nome_cliente.get()
+        cnpj = self.cp_cnpj_cliente.get()
+        telefone = self.cp_telefone_cliente.get()
+        cod_grupo = self.cp_grupo_cliente.get()
+
         try:
+
+            if self.flagOnConsulta:
+                msg_aviso = "Você precisa limpar os campos antes de cadastrar um novo cliente!"
+                return
             conn = Banco.createConnection()
 
             cursor = conn.cursor()
@@ -71,7 +95,7 @@ class CadFuncionariosContent:
                 "p4": cod_grupo
             })
             conn.commit()
-            conn.close()
+
 
         except ValueError as e:
             msg_aviso = "Campo de sálario e Qtd de dependentes aceita apenas valores numericos."
@@ -79,15 +103,22 @@ class CadFuncionariosContent:
             print(error)
             msg_aviso = "Erro ao acessar o banco de dados."
         finally:
+            conn.close()
             Utils.showMsg("Aviso!", msg_aviso)
+            self.buildTree()
 
         pass
 
-    def excluir_cliente(self, id_cliente):
+    def excluir_cliente(self):
         msg_aviso = "Cliente deletado com sucesso!!"
+        id_cliente = self.cp_id_cliente.get()
         try:
+
             if id_cliente == '':
                 msg_aviso = "ID precisa estar preenchido"
+                return
+            elif not self.flagOnConsulta:
+                msg_aviso = "Você precisa primeiro consultar um cliente"
                 return
 
             conn = Banco.createConnection()
@@ -97,12 +128,124 @@ class CadFuncionariosContent:
                 "p1": id_cliente
             })
             conn.commit()
-            conn.close()
 
         except sqlite3.Error as error:
             print(error)
             msg_aviso = "Erro ao acessar o banco de dados."
         finally:
+            conn.close()
+            self.limpar_campos()
+            self.buildTree()
             Utils.showMsg("Aviso!", msg_aviso)
+
+        pass
+
+    def buscar_cliente(self):
+        id_cliente = self.cp_id_cliente.get()
+        self.limpar_campos()
+        try:
+            if id_cliente == '':
+                Utils.showMsg("Aviso!", "ID precisa estar preenchido")
+                return
+
+            conn = Banco.createConnection()
+
+            cursor = conn.cursor()
+            cursor.execute(DataBaseUtils.SELECT_CLIENTE_BY_ID, {
+                "p1": id_cliente
+            })
+
+            rows = cursor.fetchone()
+            if rows:
+                self.cp_id_cliente.insert(0, id_cliente)
+                self.cp_nome_cliente.insert(0, rows[0])
+                self.cp_telefone_cliente.insert(0, rows[1])
+                self.cp_cnpj_cliente.insert(0, rows[2])
+                self.cp_grupo_cliente.insert(0, rows[3])
+            else:
+                Utils.showMsg("Aviso!", "Cliente não encontrado!")
+
+            conn.close()
+        except sqlite3.Error as error:
+            print(error)
+        finally:
+            self.flagOnConsulta = True
+            self.cp_id_cliente.config(state="readonly")
+            self.btn_limpar.config(state="active")
+            self.btn_atualizar.config(state="active")
+        pass
+
+    def atualizar_cliente(self):
+        try:
+            if not self.flagOnConsulta:
+                Utils.showMsg("Aviso!", "Primeiro é necessário consultar um cliente para depois alterar o seu cadastro!")
+                return
+
+            conn = Banco.createConnection()
+
+            cursor = conn.cursor()
+            cursor.execute(DataBaseUtils.UPDATE_CLIENTE_BY_ID, {
+                "p1": self.cp_nome_cliente.get(),
+                "p2": self.cp_telefone_cliente.get(),
+                "p3": self.cp_cnpj_cliente.get(),
+                "p4": self.cp_grupo_cliente.get(),
+                "p5": self.cp_id_cliente.get(),
+            })
+            conn.commit()
+            conn.close()
+        except sqlite3.Error as error:
+            print(error)
+        finally:
+            Utils.showMsg("Aviso!", "Cliente atualizado com sucesso!")
+            self.buildTree()
+        pass
+
+    def limpar_campos(self):
+        self.cp_nome_cliente.delete(0, 'end')
+        self.cp_cnpj_cliente.delete(0, 'end')
+        self.cp_telefone_cliente.delete(0, 'end')
+        self.cp_grupo_cliente.delete(0, 'end')
+        self.flagOnConsulta = False
+        self.btn_limpar.config(state="disable")
+        self.btn_atualizar.config(state="disable")
+        self.cp_id_cliente.config(state="normal")
+        self.cp_id_cliente.delete(0, 'end')
+        pass
+
+    def buildTree(self):
+        self.tree_main['columns'] = ("id", "Nome", "CNPJ", "Telefone", "CodGrupo")
+        self.tree_main.column("id", width=125, anchor=tk.N)
+        self.tree_main.column("Nome", width=125, anchor=tk.N)
+        self.tree_main.column("CNPJ", width=125, anchor=tk.N)
+        self.tree_main.column("Telefone", width=125, anchor=tk.N)
+        self.tree_main.column("CodGrupo", width=125, anchor=tk.N)
+
+        self.tree_main.heading("id", text="ID")
+        self.tree_main.heading("Nome", text="Nome Cliente")
+        self.tree_main.heading("CNPJ", text=" CNPJ/CPF")
+        self.tree_main.heading("Telefone", text="Telefone")
+        self.tree_main.heading("CodGrupo", text="Cód. Grupo")
+
+        try:
+            conn = Banco.createConnection()
+
+            cursor = conn.cursor()
+            cursor.execute(DataBaseUtils.SELECT_ALL_CLIENTES)
+
+            rows = cursor.fetchall()
+
+            # Limpando registros que já existem
+            for i in self.tree_main.get_children():
+                self.tree_main.delete(i)
+
+            # Adicionando todos os clientes existentes
+            for row in rows:
+                self.tree_main.insert(parent='', index='end', iid=row[0], text='', values=(row[0], row[1], row[2], row[3], row[4]))
+                pass
+
+            conn.close()
+
+        except sqlite3.Error as error:
+            print(error)
 
         pass
